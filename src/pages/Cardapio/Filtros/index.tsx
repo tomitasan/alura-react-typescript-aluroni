561774a9774be2d0ca2c20e812edd3b57f1be98b
Antonio Tomita <atomita@uolinc.com>
2024-02-19 17:53:22 -0300

    adicao do react-router-dom para as rotas do aluroni

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -1,6 +1,7 @@
-import styles from "./Filtros.module.scss";
-import filtros from "./filtros.json";
-import classNames from "classnames";
+import React from 'react';
+import styles from './Filtros.module.scss';
+import filtros from './filtros.json';
+import classNames from 'classnames';
 
 type IOpcao = typeof filtros[0];
 
@@ -11,23 +12,23 @@ interface Props {
 
 export default function Filtros({ filtro, setFiltro } : Props) {
 
-    function selecionarFiltro(opcao: IOpcao){
-        if(filtro === opcao.id) return setFiltro(null);
-        return setFiltro(opcao.id);
-    }
+  function selecionarFiltro(opcao: IOpcao){
+    if(filtro === opcao.id) return setFiltro(null);
+    return setFiltro(opcao.id);
+  }
 
-    return <div className={styles.filtros}>
-        {filtros.map((opcao) => (
-            <button 
-                className={classNames({
-                    [styles.filtros__filtro]: true,
-                    [styles["filtros__filtro--ativo"]] : filtro === opcao.id
-                })}
-                key={opcao.id} 
-                onClick={() => selecionarFiltro(opcao)}
-            >
-                {opcao.label}
-            </button>
-        ))}
-    </div>
+  return <div className={styles.filtros}>
+    {filtros.map((opcao) => (
+      <button 
+        className={classNames({
+          [styles.filtros__filtro]: true,
+          [styles['filtros__filtro--ativo']] : filtro === opcao.id
+        })}
+        key={opcao.id} 
+        onClick={() => selecionarFiltro(opcao)}
+      >
+        {opcao.label}
+      </button>
+    ))}
+  </div>;
 }
--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,7 +1,8 @@
-import cardapio from "./itens.json";
-import Item from "./Item";
-import styles from "./Itens.module.scss"
-import { useEffect, useState } from "react";
+import cardapio from 'data/cardapio.json';
+import Item from './Item';
+import styles from './Itens.module.scss';
+import React, { useEffect, useState } from 'react';
+import { Cardapio } from 'types/Prato';
 
 interface Props {
     busca: string;
@@ -10,43 +11,43 @@ interface Props {
 }
 
 export default function Itens(props: Props) {
-    const [lista, setLista] = useState(cardapio);
-    const { busca, filtro, ordenador } = props;
+  const [lista, setLista] = useState(cardapio);
+  const { busca, filtro, ordenador } = props;
 
-    function testaBusca(title: string){
-        // o parâmetro i dentro de RegExp indica case insensitive
-        const regex = new RegExp(busca, 'i');
-        return regex.test(title);
+  function testaBusca(title: string){
+    // o parâmetro i dentro de RegExp indica case insensitive
+    const regex = new RegExp(busca, 'i');
+    return regex.test(title);
+  }
+
+  function testaFiltro(id: number | null){
+    if(filtro !== null) return filtro === id;
+    return true;
+  }
+
+  function ordenar(novaLista: Cardapio) {
+    switch(ordenador){
+    case 'porcao': return novaLista.sort((a,b) => a.size > b.size ? 1 : -1);
+    case 'qtd_pessoas': return novaLista.sort((a,b) => a.serving > b.serving ? 1 : -1);
+    case 'preco': return novaLista.sort((a,b) => a.price > b.price ? 1 : -1);
+    default: return novaLista;
     }
+  }
 
-    function testaFiltro(id: number | null){
-        if(filtro !== null) return filtro === id;
-        return true;
-    }
+  useEffect(() => {
+    const novaLista = cardapio.filter(
+      item => testaBusca(item.title) && testaFiltro(item.category.id));
+    setLista(ordenar(novaLista));
+  }, [busca, filtro, ordenador]);
 
-    function ordenar(novaLista: typeof cardapio) {
-        switch(ordenador){
-            case 'porcao': return novaLista.sort((a,b) => a.size > b.size ? 1 : -1);
-            case 'qtd_pessoas': return novaLista.sort((a,b) => a.serving > b.serving ? 1 : -1);
-            case 'preco': return novaLista.sort((a,b) => a.price > b.price ? 1 : -1);
-            default: return novaLista;
-        }
-    }
-
-    useEffect(() => {
-        const novaLista = cardapio.filter(
-            item => testaBusca(item.title) && testaFiltro(item.category.id));
-        setLista(ordenar(novaLista));
-    }, [busca, filtro, ordenador]);
-
-    return (
-        <div className={styles.itens}>
-            {lista.map(item => (
-                <Item 
-                    key={item.id} 
-                    {...item}
-                />
-            ))}
-        </div>
-    )
+  return (
+    <div className={styles.itens}>
+      {lista.map(item => (
+        <Item 
+          key={item.id} 
+          {...item}
+        />
+      ))}
+    </div>
+  );
 }
